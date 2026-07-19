f745c78e8f63eec8407d423adaf8d42242482908
edyeo82 <101386993+edyeo82@users.noreply.github.com>
2026-07-19 17:54:52 +0800

    Cache Ting Xie app modules

--- tingxie/sw.js
+++ tingxie/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'tingxie-shell-v1';
-const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
+const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './app-core.js', './app-dictation.js', './app-marking.js', './app-init.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
