15efe038992218a43f585ad123fdccfe9474eb1e
edyeo82 <101386993+edyeo82@users.noreply.github.com>
2026-07-19 19:37:44 +0800

    Refresh Ting Xie OCR cache

--- tingxie/sw.js
+++ tingxie/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'tingxie-shell-v2';
-const APP_SHELL = ['./', './index.html', './styles.css', './camera.css', './app.js', './app-core.js', './app-camera.js', './app-dictation.js', './app-marking.js', './app-init.js', './manifest.webmanifest', './icon.svg'];
+const CACHE_NAME = 'tingxie-shell-v3';
+const APP_SHELL = ['./', './index.html', './styles.css', './camera.css', './app.js', './app-core.js', './app-ocr.js', './app-camera.js', './app-dictation.js', './app-marking.js', './app-init.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
